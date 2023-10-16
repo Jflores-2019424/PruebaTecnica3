ef765fd652949ec7eaaee975804bd3c84ec1429b
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-10-15 18:39:17 -0600

    base

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT;
 const cors = require("cors");
 
 const routesUser = require('./src/routes/user.routes')
+const routesMovies = require('./src/routes/movies.routes')
+const routesTickets = require('./src/routes/ticket.routes')
 connection();
 
 app.use(express.urlencoded({extended: false}));
@@ -15,7 +17,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', routesUser);
+app.use('/api', routesUser, routesMovies, routesTickets);
 
 app.listen(port, ()=> {
     console.log(`Servidor corriendo en el puerto ${port}`);
